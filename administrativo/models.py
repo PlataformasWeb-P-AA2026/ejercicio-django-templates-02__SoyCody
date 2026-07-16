9a931a26776748120e16b60e529ea101cc1f8a0c
Cody <codycabreraa@gmail.com>
2026-07-16 11:11:52 -0500

    Gasto

--- administrativo/models.py
+++ administrativo/models.py
@@ -1,14 +1,10 @@
-from django.db import models
-
-# Create your models here.
-
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     cedula = models.CharField(max_length=30, unique=True)
     correo = models.EmailField()
 
-    def __str__(self):
+    def _str_(self):
         return "%s %s %s %s" % (self.nombre,
                 self.apellido,
                 self.cedula,
@@ -26,11 +22,19 @@ class Estudiante(models.Model):
                 valor = "Pichincha"
         return valor
 
+    def obtener_valorTelefonos(self):
+        valor = 0 
+        for num in self.numeros_telefonicos.all():
+            valor = valor + num.valor_mensual
+        return valor
+
+
 class NumeroTelefonico(models.Model):
     telefono = models.CharField(max_length=100)
     tipo = models.CharField(max_length=100)
+    valor_mensual = models.FloatField(default=0.0) 
     estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE,
             related_name="numeros_telefonicos")
 
-    def __str__(self):
+    def _str_(self):
         return "%s %s" % (self.telefono, self.tipo)
